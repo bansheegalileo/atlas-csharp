29fd65d4708678f9cd4da32c8ebf3f91e2369a1c
bansheegalileo <venable.rin@gmail.com>
2024-01-10 12:53:41 -0600

    eeeeep ;

--- csharp-hello_world/11-concat_edges/obj/Debug/net8.0/11-concat_edges.AssemblyInfo.cs
+++ csharp-hello_world/11-concat_edges/obj/Debug/net8.0/11-concat_edges.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11-concat_edges")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1079abc42e6d17dd642a00b772943ccad0183d")]
 [assembly: System.Reflection.AssemblyProductAttribute("11-concat_edges")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11-concat_edges")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- csharp-hello_world/5-print_float/obj/Debug/net8.0/5-print_float.AssemblyInfo.cs
+++ csharp-hello_world/5-print_float/obj/Debug/net8.0/5-print_float.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1079abc42e6d17dd642a00b772943ccad0183d")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
