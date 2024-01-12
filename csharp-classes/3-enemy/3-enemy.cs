2cc5414182e1f3f2f9886515cecc07adb2d02595
bansheegalileo <venable.rin@gmail.com>
2024-01-12 05:40:26 -0600

    afwef

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -1,4 +1,6 @@
-﻿namespace Enemies
+﻿using System;
+
+namespace Enemies
 {
     public class Zombie
     {
--- csharp-classes/4-enemy/4-enemy.cs
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -1,4 +1,6 @@
-﻿namespace Enemies
+﻿using System;
+
+namespace Enemies
 {
     public class Zombie
     {
@@ -13,7 +15,7 @@
         public Zombie(int value)
         {
             if (value < 0)
-                throw new ArgumentException("Health must be greater than or equal to 0");
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
 
             health = value;
         }
@@ -27,6 +29,11 @@
         public int GetHealth()
         {
             return health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
         }
     }
 }
