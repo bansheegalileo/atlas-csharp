44236045f81d35ef9bd4f75c9d8bd2440a9be53a
bansheegalileo <venable.rin@gmail.com>
2024-01-12 03:57:19 -0600

    lkjhgdsd

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestStudent = "";
+        int bestScore = int.MinValue;
+
+        foreach (var entry in myList)
+        {
+            if (entry.Value > bestScore)
+            {
+                bestStudent = entry.Key;
+                bestScore = entry.Value;
+            }
+        }
+
+        return bestStudent;
+    }
+}
