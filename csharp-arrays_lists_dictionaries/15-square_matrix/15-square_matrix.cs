d032befc565b242ab797b8e995c23d83d909956c
bansheegalileo <venable.rin@gmail.com>
2024-01-12 04:07:05 -0600

    jghgfd

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -18,38 +18,4 @@ class Matrix
 
         return resultMatrix;
     }
-
-    static void Main()
-    {
-        int[,] originalMatrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-
-        int[,] squaredMatrix = Square(originalMatrix);
-
-        Console.WriteLine("Original Matrix:");
-        PrintMatrix(originalMatrix);
-
-        Console.WriteLine();
-
-        Console.WriteLine("Squared Matrix:");
-        PrintMatrix(squaredMatrix);
-    }
-
-    static void PrintMatrix(int[,] matrix)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                Console.Write($"{matrix[i, j]} ");
-            }
-            Console.WriteLine();
-        }
-    }
 }
