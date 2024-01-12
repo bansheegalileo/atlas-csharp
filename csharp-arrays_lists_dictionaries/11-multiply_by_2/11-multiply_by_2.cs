2a91cd28e9b470a4d15970c7b04b7ea67ace5f35
bansheegalileo <venable.rin@gmail.com>
2024-01-12 03:51:39 -0600

    dfgse

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> multipliedDict = new Dictionary<string, int>();
+
+        foreach (var kvp in myDict)
+        {
+            multipliedDict[kvp.Key] = kvp.Value * 2;
+        }
+
+        return multipliedDict;
+    }
+}
