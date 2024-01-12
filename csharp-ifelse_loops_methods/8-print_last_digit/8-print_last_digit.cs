2ea239de6b01fc51e590902688befec039069aba
bansheegalileo <venable.rin@gmail.com>
2024-01-11 23:34:09 -0600

    testing

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,9 @@
+﻿public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = Math.Abs(number % 10);
+        Console.WriteLine(lastDigit);
+        return lastDigit;
+    }
+}
