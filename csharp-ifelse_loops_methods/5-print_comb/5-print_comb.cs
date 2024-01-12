3b8f4ffc54e03a50da825f82cf1746772875fd1d
bansheegalileo <venable.rin@gmail.com>
2024-01-11 23:04:49 -0600

    5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number < 100; number++)
+        {
+            Console.Write($"{number:D2}");
+
+            if (number < 99)
+            {
+                Console.Write(", ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
