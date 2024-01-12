87c348adf2ef736960b31c980d08321256c827c2
bansheegalileo <venable.rin@gmail.com>
2024-01-11 23:17:42 -0600

    dkjytd

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int tens = 0; tens < 9; tens++)
+        {
+            for (int ones = tens + 1; ones < 10; ones++)
+            {
+                Console.Write($"{tens}{ones:D1}{(tens < 8 ? ", " : Environment.NewLine)}");
+            }
+        }
+    }
+}
