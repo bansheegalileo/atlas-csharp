e5dfdf077c45ac955ff8a6a7ba541ef85561a29c
bansheegalileo <venable.rin@gmail.com>
2024-01-12 04:52:01 -0600

    kjghfd

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        try
+        {
+            int count = 0;
+            foreach (int item in myList)
+            {
+                if (count < n)
+                {
+                    Console.WriteLine(item);
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return count;
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
+    }
+}
---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        try
+        {
+            for (int i = 0; i < listLength; i++)
+            {
+                int value1 = i < list1.Count ? list1[i] : 0;
+                int value2 = i < list2.Count ? list2[i] : 0;
+
+                if (value2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+
+                result.Add(value1 / value2);
+            }
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            result.Clear();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Out of range");
+            result.Clear();
+        }
+
+        return result;
+    }
+}
