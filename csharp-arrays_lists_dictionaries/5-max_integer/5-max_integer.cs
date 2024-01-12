8bcde5138f3c52b19bea28b3d98928cd5923f407
bansheegalileo <venable.rin@gmail.com>
2024-01-12 03:32:48 -0600

    5

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+
+        int max = myList[0];
+        foreach (int num in myList)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+}
