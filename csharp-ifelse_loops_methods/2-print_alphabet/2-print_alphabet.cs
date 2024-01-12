47d2b2b01407faf1dfa9ee7b8d40565809015a3a
bansheegalileo <venable.rin@gmail.com>
2024-01-11 22:48:17 -0600

    2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            Console.Write(letter);
+        }
+
+        Console.WriteLine();
+    }
+}
