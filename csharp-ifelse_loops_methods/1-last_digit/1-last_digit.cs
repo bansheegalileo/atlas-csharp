f00222fc096e7817a91d1c1a059d02f3fd3cea04
bansheegalileo <venable.rin@gmail.com>
2024-01-11 22:40:59 -0600

    changes to one

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -7,7 +7,7 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
 
-        int lastDigit = Math.Abs(number) % 10;
+        int lastDigit = Math.Abs(number) % 10 * (Math.Sign(number));
 
         Console.Write($"The last digit of {number} is {lastDigit} and ");
 
